1b44db27b0314ad1e3e92c4458d207ba9b1b7430
Martha Mwangi <37874101+Maraqua@users.noreply.github.com>
2020-04-15 22:48:35 +0300

    Updated DatamanagerTest.Java

--- app/src/test/java/com/marty/unitkeeper/DataManagerTest.java
+++ app/src/test/java/com/marty/unitkeeper/DataManagerTest.java
@@ -10,5 +10,20 @@ class DataManagerTest {
     @Test
     public
     void createNewNote () {
+        DataManager dm = DataManager.getInstance ();
+        final CourseInfo course = dm.getCourse ( "android_async" );
+        final String noteTitle = "Test Note Title";
+        final String noteText = "This is Note Text for test";
+
+        int noteIndex = dm.createNewNote ();
+        NoteInfo newNote = dm.getNotes ().get(noteIndex);
+        newNote.setCourse ( course );
+        newNote.setTitle ( noteTitle );
+        newNote.setText ( noteText );
+
+        NoteInfo compareNote = dm.getNotes ().get ( noteIndex );
+        assertEquals ( compareNote.getCourse (),course );
+        assertEquals ( compareNote.getTitle (),noteTitle );
+        assertEquals ( compareNote.getText (),noteText );
     }
 }
